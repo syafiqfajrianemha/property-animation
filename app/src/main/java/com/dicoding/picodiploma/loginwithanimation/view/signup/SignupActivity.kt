89d67f75643f9fd15b26e08e7fa0f6be62aca905
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2023-09-16 05:48:17 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/loginwithanimation/view/signup/SignupActivity.kt
@@ -0,0 +1,81 @@
+package com.dicoding.picodiploma.loginwithanimation.view.signup
+
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
+import android.os.Build
+import android.os.Bundle
+import android.view.View
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.picodiploma.loginwithanimation.databinding.ActivitySignupBinding
+
+class SignupActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignupBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupView()
+        setupAction()
+        playAnimation()
+    }
+
+    private fun setupView() {
+        @Suppress("DEPRECATION")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
+        supportActionBar?.hide()
+    }
+
+    private fun setupAction() {
+        binding.signupButton.setOnClickListener {
+            val email = binding.emailEditText.text.toString()
+
+            AlertDialog.Builder(this).apply {
+                setTitle("Yeah!")
+                setMessage("Akun dengan $email sudah jadi nih. Yuk, login dan belajar coding.")
+                setPositiveButton("Lanjut") { _, _ ->
+                    finish()
+                }
+                create()
+                show()
+            }
+        }
+    }
+
+    private fun playAnimation() {
+        ObjectAnimator.ofFloat(binding.imageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }.start()
+
+        val title = ObjectAnimator.ofFloat(binding.titleTextView, View.ALPHA, 1f).setDuration(1000)
+
+        val name = ObjectAnimator.ofFloat(binding.nameTextView, View.ALPHA, 1f).setDuration(1000)
+        val nameLayout = ObjectAnimator.ofFloat(binding.nameEditTextLayout, View.ALPHA, 1f).setDuration(1000)
+
+        val email = ObjectAnimator.ofFloat(binding.emailTextView, View.ALPHA, 1f).setDuration(1000)
+        val emailLayout = ObjectAnimator.ofFloat(binding.emailEditTextLayout, View.ALPHA, 1f).setDuration(1000)
+
+        val password = ObjectAnimator.ofFloat(binding.passwordTextView, View.ALPHA, 1f).setDuration(1000)
+        val passwordLayout = ObjectAnimator.ofFloat(binding.passwordEditTextLayout, View.ALPHA, 1f).setDuration(1000)
+
+        val signup = ObjectAnimator.ofFloat(binding.signupButton, View.ALPHA, 1f).setDuration(1000)
+
+        AnimatorSet().apply {
+            playSequentially(title, name, nameLayout, email, emailLayout, password, passwordLayout, signup)
+            start()
+        }
+    }
+}
